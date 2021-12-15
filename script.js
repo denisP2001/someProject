c9ecb4b48001fab1c8c9df425220ba74ba5bea5a
Denis <pronindenis1978@gmail.com>
2021-12-15 21:20:44 +0300

    added input validation and classification by movie counter

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -8,12 +10,34 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-b = prompt('На сколько оцените его?', ''),
-c = prompt('Один из последних просмотренных фильмов?', ''),
-d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        console.log('done');
+        personalMovieDB.movies[a] = b;
+
+    }
+    else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотренно довольно мало фильмов');
+}
+else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+}
+else if (personalMovieDB.count > 30) {
+    alert('Вы киноман!');
+}
+else {
+    alert('Произошла ошибка!');
+}
+
+
 
 console.log(personalMovieDB);
